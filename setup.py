6ec2e073e644345bb4892738a7739d362fda3e14
Robin Siep <RobinSiep1@gmail.com>
2020-05-26 09:27:58 +0200

    setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+requires = [
+    'fastapi',
+    'uvicorn'
+]
+
+setup(
+    name='covert',
+    version='0.1',
+    author='Code R',
+    author_email='hello@coderstudio.nl',
+    install_requires=requires
+)
