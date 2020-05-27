8af8bac842af32ab39db974227d141f176defdec
Robin Siep <RobinSiep1@gmail.com>
2020-05-27 09:33:10 +0200

    read and update settings

--- apollo/__init__.py
+++ apollo/__init__.py
@@ -1,7 +1,23 @@
+from configparser import ConfigParser
+
 from fastapi import FastAPI
 
 from apollo.handlers import root
+from apollo.lib.settings import update_settings
+from apollo.models import init_sqlalchemy
 
 app = FastAPI()
 
 app.include_router(root.router)
+
+
+def configure():
+    read_settings_files()
+    init_sqlalchemy()
+
+
+def read_settings_files():
+    config = ConfigParser()
+    config.read('settings.ini')
+    config.read('local-settings.ini')
+    update_settings(config)
---
+++ apollo/lib/settings.py
@@ -0,0 +1,6 @@
+settings = dict()
+
+
+def update_settings(settings_):
+    settings.update(settings_)
+    return settings
