07f9e7bc659248233287acc96a7645fca2566b87
Robin Siep <RobinSiep1@gmail.com>
2020-06-02 14:19:43 +0200

    duplication test

--- tests/handlers/test_agent.py
+++ tests/handlers/test_agent.py
@@ -5,3 +5,11 @@ def test_post_agent_success(test_client, db_session):
     )
 
     assert response.status_code == 201
+
+
+def test_post_agent_name_exists(test_client, db_session):
+    agent = {'name': 'test'}
+    test_client.post('/agent', json=agent)
+    response = test_client.post('/agent', json=agent)
+
+    assert response.status_code == 422
