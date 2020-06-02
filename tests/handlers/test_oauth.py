faf71e03ac33151d1dc73bff132386a019eba2e5
Robin Siep <RobinSiep1@gmail.com>
2020-06-02 16:38:26 +0200

    test no auth header

--- tests/handlers/test_oauth.py
+++ tests/handlers/test_oauth.py
@@ -1,4 +1,5 @@
-def test_post_access_token_success(test_client, db_session):
+def test_post_access_token_no_auth_header(test_client, db_session):
     response = test_client.post('/oauth/token')
 
-    assert response.status_code == 201
+    assert response.status_code == 400
+    assert response.json() == {'detail': "No authorization header found"}
