6d6e0c8b9c37df9d0fd2d9f6517d11635f071140
Robin Siep <RobinSiep1@gmail.com>
2020-06-02 15:50:10 +0200

    create oauth client for agent

--- apollo/handlers/agent.py
+++ apollo/handlers/agent.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from apollo.lib.schemas.agent import CreateAgentSchema
 from apollo.models import get_session, save
 from apollo.models.agent import Agent
+from apollo.models.oauth import OAuthClient
 
 router = APIRouter()
 
@@ -11,4 +12,7 @@ router = APIRouter()
 @router.post("/agent", status_code=201)
 def post_agent(agent_data: CreateAgentSchema,
                session: Session = Depends(get_session)):
-    save(session, Agent(**dict(agent_data)))
+    save(session, Agent(
+        oauth_client=OAuthClient(client_type='confidential'),
+        **dict(agent_data)
+    ))
--- apollo/models/agent.py
+++ apollo/models/agent.py
@@ -2,6 +2,7 @@ import uuid
 
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from apollo.models import Base
 
@@ -11,6 +12,8 @@ class Agent(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String(100), unique=True, nullable=False)
+
+    oauth_client = relationship('OAuthClient', uselist=False)
 
 
 def get_agent_by_name(session, name):
