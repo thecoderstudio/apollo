911d1dfe9aabb42797be5f7c7080f892557b6916
Robin Siep <RobinSiep1@gmail.com>
2020-06-02 15:04:37 +0200

    rollback test

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -1,10 +1,12 @@
 import uuid
+from unittest.mock import Mock
 
+import pytest
 from sqlalchemy.orm.session import Session
 
 import apollo.models
 from apollo.models import (get_connection_url, get_session, init_sqlalchemy,
-                           persist, rollback)
+                           persist, rollback, commit, save)
 from apollo.models.agent import Agent
 
 
@@ -55,3 +57,34 @@ def test_rollback(db_session):
     rollback(db_session)
     agent = db_session.query(Agent).get(agent_id)
     assert agent is None
+
+
+def test_commit(db_session):
+    agent_id = uuid.uuid4()
+    db_session.add(Agent(id=agent_id, name='test'))
+    commit(db_session)
+    agent = db_session.query(Agent).get(agent_id)
+    assert agent is not None
+
+
+def test_save_success(db_session):
+    agent_copy, agent_id = save(db_session, Agent(name='test'))
+    agent = db_session.query(Agent).get(agent_id)
+    assert agent_copy.id == agent.id
+    assert agent.id == agent_id
+
+
+def test_save_no_id(mocker, db_session):
+    mocker.patch('apollo.models.persist', return_value=Mock(spec=[]))
+    agent_copy, agent_id = save(db_session, Agent(name='test'))
+    assert agent_id is None
+
+
+def test_save_rollback(mocker, db_session):
+    mocker.patch('apollo.models.persist', side_effect=Exception('test'))
+    agent_id = uuid.uuid4()
+
+    with pytest.raises(Exception):
+        save(db_session, Agent(id=agent_id, name='test'))
+
+    assert db_session.query(Agent).get(agent_id) is None
