847cc7c86c5c55b9254bd61b3b96f6732f73ce7e
Robin Siep <RobinSiep1@gmail.com>
2020-06-03 13:27:54 +0200

    client by creds test

--- tests/models/test_oauth.py
+++ tests/models/test_oauth.py
@@ -1,7 +1,13 @@
 import datetime
+import uuid
+from secrets import token_hex
+
+import pytest
+from sqlalchemy.orm.exc import NoResultFound
 
 from apollo.models.agent import Agent
-from apollo.models.oauth import OAuthAccessToken, OAuthClient
+from apollo.models.oauth import (OAuthAccessToken, OAuthClient,
+                                 get_client_by_creds)
 
 
 def test_oauth_access_token_expired():
@@ -44,3 +50,23 @@ def test_oauth_client_cascades(db_session):
     assert len(db_session.query(OAuthAccessToken).all()) == 0
     assert len(db_session.query(OAuthClient).all()) == 0
     assert db_session.query(Agent).get(agent_id) is not None
+
+
+def test_get_client_by_creds(db_session):
+    agent = Agent(
+        name='test',
+        oauth_client=OAuthClient(client_type='confidential')
+    )
+    db_session.add(agent)
+    db_session.flush()
+    agent_id = agent.id
+    client_secret = agent.oauth_client.client_secret
+    db_session.commit()
+
+    client = get_client_by_creds(db_session, agent_id, client_secret)
+    assert client.agent_id == agent_id
+
+
+def test_get_client_by_creds_not_found(db_session):
+    with pytest.raises(NoResultFound):
+        get_client_by_creds(db_session, uuid.uuid4(), token_hex(32))
