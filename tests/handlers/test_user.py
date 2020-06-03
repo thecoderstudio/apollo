bf2145fe0f19af1986cefd85f172f01f4f58e27b
rikvanderwerf <rikvanderwerf@invyte.app>
2020-06-03 14:37:44 +0200

    check duplicate username

--- tests/handlers/test_user.py
+++ tests/handlers/test_user.py
@@ -8,10 +8,25 @@ def test_post_user_successful(test_client, db_session):
     assert response.json()['username'] == 'doejohn'
 
     
-def test_post_user_unsuccessful(test_client, db_session):
+def test_post_user_short_password(test_client, db_session):
     response = test_client.post(
         '/user',
         json={'username': 'doejohn', 'password': '123'},
+    )
+
+    assert response.status_code == 422
+
+def test_post_user_duplicate_username(test_client, db_session):
+    response = test_client.post(
+        '/user',
+        json={'username': 'doejohn', 'password': 'testing123'},
+    )
+
+    assert response.status_code == 200
+
+    response = test_client.post(
+        '/user',
+        json={'username': 'doejohn', 'password': 'testing123'},
     )
 
     assert response.status_code == 422
