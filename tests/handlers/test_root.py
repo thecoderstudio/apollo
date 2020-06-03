f3080fa2302f595ef8f2b9cadb505594cb67d412
rikvanderwerf <rikvanderwerf@invyte.app>
2020-06-03 10:39:35 +0200

    works with version

--- tests/handlers/test_root.py
+++ tests/handlers/test_root.py
@@ -1,7 +1,7 @@
-# def test_correct_version(test_client):
-#     expected_version = '0.1'
+def test_correct_version(test_client):
+    expected_version = '0.1'
 
-#     response = test_client.get("/")
+    response = test_client.get("/")
 
-#     assert response.status_code == 200
-#     assert response.json() == {'version': expected_version}
+    assert response.status_code == 200
+    assert response.json() == {'version': expected_version}
