756ed748addf519c545fd6204622d34aac63f6ba
rikvanderwerf <rikvanderwerf@invyte.app>
2020-06-03 14:58:00 +0200

    more tests

--- tests/lib/test_hash.py
+++ tests/lib/test_hash.py
@@ -6,4 +6,13 @@ def test_password_hash():
     assert compare_plaintext_to_hash('password', password_hash, password_salt) == True
     assert compare_plaintext_to_hash('fakepassword', password_hash, password_salt) == False
 
+def test_password_hash_no_salt():
+    password_hash, _ = hash_plaintext('password')
+
+    assert compare_plaintext_to_hash('password', password_hash) == False
+
+def test_hash_no_password():
+    password_hash, _ = hash_plaintext('password')
+
+    assert compare_plaintext_to_hash(None, password_hash) == False
 
