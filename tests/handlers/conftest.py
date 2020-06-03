701e38cf63055a69372f0b44a2fe89537a2d6d18
rikvanderwerf <rikvanderwerf@invyte.app>
2020-06-03 14:29:16 +0200

    removed more unused code

--- tests/handlers/conftest.py
+++ tests/handlers/conftest.py
@@ -1,14 +1,8 @@
 from fastapi.testclient import TestClient
 from pytest import fixture
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import close_all_sessions
-from sqlalchemy_utils.functions import database_exists, create_database
 
 from apollo import app
-from apollo.lib.settings import settings
-from apollo.models.user import User
 
 @fixture
-def test_client(monkeypatch):
+def test_client():
     return TestClient(app)
