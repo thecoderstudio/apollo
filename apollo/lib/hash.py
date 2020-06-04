e488ef504489e96d1223571679b225112d1059eb
rikvanderwerf <rikvanderwerf@invyte.app>
2020-06-04 09:46:46 +0200

    100% test coverage

--- apollo/lib/hash.py
+++ apollo/lib/hash.py
@@ -7,14 +7,10 @@ def hash_plaintext(plaintext: str):
             salt.decode('utf-8'))
 
 
-def compare_plaintext_to_hash(plaintext: str, hashed_plaintext: str = None, salt: str =None):
-    if not salt:
-        salt = bcrypt.gensalt().decode('utf-8')
-
-    new_hashed_plaintext = bcrypt.hashpw(plaintext.encode('utf-8'),
-                                         salt.encode('utf-8'))
-
+def compare_plaintext_to_hash(plaintext: str, hashed_plaintext: str, salt: str):
     try:
+        new_hashed_plaintext = bcrypt.hashpw(plaintext.encode('utf-8'),
+                                         salt.encode('utf-8'))
         if new_hashed_plaintext == hashed_plaintext.encode('utf-8'):
             return True
     except AttributeError:
--- tests/lib/test_hash.py
+++ tests/lib/test_hash.py
@@ -1,18 +1,13 @@
 from apollo.lib.hash import hash_plaintext, compare_plaintext_to_hash
 
 def test_password_hash():
-    password_hash, password_salt = hash_plaintext('password')
+    password_hash, salt = hash_plaintext('password')
 
-    assert compare_plaintext_to_hash('password', password_hash, password_salt) == True
-    assert compare_plaintext_to_hash('fakepassword', password_hash, password_salt) == False
-
-def test_password_hash_no_salt():
-    password_hash, _ = hash_plaintext('password')
-
-    assert compare_plaintext_to_hash('password', password_hash) == False
+    assert compare_plaintext_to_hash('password', password_hash, salt) == True
+    assert compare_plaintext_to_hash('fakepassword', password_hash, salt) == False
 
 def test_hash_no_password():
-    password_hash, _ = hash_plaintext('password')
+    password_hash, salt = hash_plaintext('password')
 
-    assert compare_plaintext_to_hash(None, password_hash) == False
+    assert compare_plaintext_to_hash(None, password_hash, salt) == False
 
