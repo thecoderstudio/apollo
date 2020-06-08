16242ca0f6b483b6f40f64784650a79c32a06b83
Robin Siep <RobinSiep1@gmail.com>
2020-06-08 12:06:26 +0200

    add secure router to other endpoints

--- apollo/handlers/agent.py
+++ apollo/handlers/agent.py
@@ -3,14 +3,16 @@ from sqlalchemy.orm import Session
 
 from apollo.lib.router import SecureRouter
 from apollo.lib.schemas.agent import AgentSchema, CreateAgentSchema
+from apollo.lib.security import Allow
 from apollo.models import get_session, save
 from apollo.models.agent import Agent
 from apollo.models.oauth import OAuthClient
 
-router = SecureRouter()
+router = SecureRouter([(Allow, 'role:admin', 'agent.post')])
 
 
-@router.post("/agent", status_code=201, response_model=AgentSchema)
+@router.post("/agent", status_code=201, response_model=AgentSchema,
+             permission='agent.post')
 def post_agent(agent_data: CreateAgentSchema,
                session: Session = Depends(get_session)):
     agent, _ = save(session, Agent(
--- apollo/handlers/user.py
+++ apollo/handlers/user.py
@@ -1,15 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from apollo.lib.hash import hash_plaintext
+from apollo.lib.router import SecureRouter
 from apollo.lib.schemas.user import CreateUserSchema, UserSchema
+from apollo.lib.security import Allow
 from apollo.models import get_session, save
 from apollo.models.user import User
 
-router = APIRouter()
+router = SecureRouter([(Allow, 'role:admin', 'user.post')])
 
 
-@router.post('/user', status_code=201, response_model=UserSchema)
+@router.post('/user', status_code=201, response_model=UserSchema,
+             permission='user.post')
 def post_user(user_data: CreateUserSchema,
               session: Session = Depends(get_session)):
     data = user_data.dict()
