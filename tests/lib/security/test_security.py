fe729a43fec684b9d5d3b476ef44e37633fadf35
Robin Siep <RobinSiep1@gmail.com>
2020-06-09 13:16:53 +0200

    default user principal test

--- tests/lib/security/test_security.py
+++ tests/lib/security/test_security.py
@@ -86,14 +86,25 @@ def test_auth_policy_agent_principals(mock_policy, access_token,
                           f"agent:{access_token.client_id}"]
 
 
-def test_auth_policy_user_principals(mock_policy, mock_http_connection,
-                                     user, session_cookie):
+def test_auth_policy_admin_user_principals(mock_policy, mock_http_connection,
+                                           user, session_cookie):
     principals = mock_policy().get_principals(mock_http_connection(
         cookies=session_cookie
     ))
 
     assert principals == [Everyone, Authenticated, Human, f"user:{user.id}",
                           'role:admin']
+
+
+def test_auth_policy_default_user_principals(mock_policy, mock_http_connection,
+                                             user, session_cookie, db_session):
+    user.role = None
+    db_session.commit()
+    principals = mock_policy().get_principals(mock_http_connection(
+        cookies=session_cookie
+    ))
+
+    assert principals == [Everyone, Authenticated, Human, f"user:{user.id}"]
 
 
 @pytest.mark.parametrize('auth_header', [
