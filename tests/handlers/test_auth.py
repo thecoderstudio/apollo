11ef325169a03c6f28e546dad50dc6100bddcf1a
Robin Siep <RobinSiep1@gmail.com>
2020-06-09 16:28:54 +0200

    login handler success test

---
+++ tests/handlers/test_auth.py
@@ -0,0 +1,23 @@
+from apollo.lib.hash import hash_plaintext
+from apollo.models.user import User
+
+
+def test_login_success(test_client, db_session):
+    username = 'testuser'
+    password = 'testing123'
+    password_hash, password_salt = hash_plaintext(password)
+
+    db_session.add(User(
+        username=username,
+        password_hash=password_hash,
+        password_salt=password_salt
+    ))
+    db_session.commit()
+
+    response = test_client.post('/auth/login', json={
+        'username': username,
+        'password': password
+    })
+
+    assert response.status_code == 200
+    # TODO assert cookies
