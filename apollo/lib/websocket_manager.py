997d1e4543fabbabd1b9886ef4b93f15ac59cdc3
rikvanderwerf <rikvanderwerf@invyte.app>
2020-06-10 14:21:42 +0200

    sending json

--- apollo/lib/websocket_manager.py
+++ apollo/lib/websocket_manager.py
@@ -14,6 +14,7 @@ class WebSocketManager(metaclass=Singleton):
     async def add_and_connect_websocket(self, websocket: WebSocket):
         self.connections[uuid.uuid4()] = websocket
         await websocket.accept()
+        await websocket.send_json("Connection accepted")
         await websocket.receive()
 
     async def send_message(self, websocket_id: uuid.UUID, message: str):
@@ -22,5 +23,5 @@ class WebSocketManager(metaclass=Singleton):
 
     async def close_and_remove_connection(self, websocket_id):
         websocket = self.connections.pop(websocket_id)
-        await self.connections[websocket_id].send_text("Closing connection.")
+        await self.connections[websocket_id].send_json("Closing connection.")
         await websocket.close()
