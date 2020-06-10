752f149fc7abb679cc5be9ffa3bdf20564148652
Robin Siep <RobinSiep1@gmail.com>
2020-06-10 09:45:23 +0200

    some test abstraction

--- tests/handlers/test_auth.py
+++ tests/handlers/test_auth.py
@@ -3,6 +3,25 @@ from apollo.models.user import User
 
 
 def test_login_success(test_client, db_session):
+    username, password = create_test_user(db_session)
+    response = test_client.post('/auth/login', json={
+        'username': username,
+        'password': password
+    })
+
+    assert response.status_code == 200
+    assert response.cookies['session'] is not None
+
+
+def test_login_wrong_credentials(test_client, db_session, username, password):
+    create_test_user(db_session)
+    response = test_client.post('/auth/login', json={
+        'username': username,
+        'password': password
+    })
+
+
+def create_test_user(db_session):
     username = 'testuser'
     password = 'testing123'
     password_hash, password_salt = hash_plaintext(password)
@@ -13,11 +32,4 @@ def test_login_success(test_client, db_session):
         password_salt=password_salt
     ))
     db_session.commit()
-
-    response = test_client.post('/auth/login', json={
-        'username': username,
-        'password': password
-    })
-
-    assert response.status_code == 200
-    assert response.cookies['session'] is not None
+    return (username, password)
