b819c519d0e10c044dbe084fdb0385324559da46
rikvanderwerf <rikvanderwerf@invyte.app>
2020-06-11 17:25:35 +0200

    tests succeed

--- apollo/lib/websocket_manager.py
+++ apollo/lib/websocket_manager.py
@@ -7,19 +7,20 @@ from typing import Dict
 
 from apollo.lib.singleton import Singleton
 
+
 class WebSocketManager(metaclass=Singleton):
     def __init__(self):
         self.connections: Dict[uuid.UUID, WebSocket] = {}
 
     async def add_and_connect_websocket(self, websocket: WebSocket):
         self.connections[uuid.uuid4()] = websocket
+
         await websocket.accept()
         await websocket.send_json("Connection accepted")
-        try: 
-            await websocket.receive_json() 
+        try:
+            return await websocket.receive()
         except WebSocketDisconnect:
             return
-
 
     async def send_message(self, websocket_id: uuid.UUID, message: str):
         websocket = self.connections[websocket_id]
@@ -28,13 +29,13 @@ class WebSocketManager(metaclass=Singleton):
     async def close_and_remove_connection(self, websocket_id):
         websocket = self.connections.pop(websocket_id)
         try:
-            await self.connections[websocket_id].send_json("Closing connection")
+            await websocket.send_json("Closing connection")
             await websocket.close()
         except RuntimeError:
             # Connection already closed
             return
 
     async def close_and_remove_all_connections(self):
-        for connection in self.connections.values():
-            await connection.close()
+        for id_ in list(self.connections):
+            await self.close_and_remove_connection(id_)
         self.connections = {}
--- tests/handlers/test_websocket.py
+++ tests/handlers/test_websocket.py
@@ -5,15 +5,20 @@ import pytest
 from fastapi import APIRouter
 from fastapi.websockets import WebSocket
 
+import asyncio
 from apollo import app
 from apollo.lib.websocket_manager import WebSocketManager
 
 
-def test_shell(test_client, authenticated_agent_headers):
+@pytest.mark.asyncio
+async def test_shell(test_client, authenticated_agent_headers):
     with test_client.websocket_connect(
         '/ws', headers=authenticated_agent_headers
     ) as websocket:
         assert websocket.receive_json() == "Connection accepted"
+
+    websocket_manager = WebSocketManager()
+    await websocket_manager.close_and_remove_all_connections()
 
 
 def test_shell_unauthenticated(test_client):
@@ -25,36 +30,30 @@ def test_shell_unauthenticated(test_client):
 async def test_web_socket_manager_add(test_client):
     websocket_manager = WebSocketManager()
 
-    @app.websocket_route('/test')
+    @app.websocket_route('/websocket_mock')
     async def mock(websocket: WebSocket):
         await websocket_manager.add_and_connect_websocket(websocket)
+
+    with test_client.websocket_connect('/websocket_mock') as websocket:
         assert len(websocket_manager.connections) == 1
 
-    @app.websocket_route('/test2')
-    async def mock(websocket: WebSocket):
-        await websocket_manager.add_and_connect_websocket(websocket)
+    with test_client.websocket_connect('/websocket_mock') as websocket:
         assert len(websocket_manager.connections) == 2
 
-    test_client.websocket_connect('/test')
-    test_client.websocket_connect('/test2')
     await websocket_manager.close_and_remove_all_connections()
 
 
 @pytest.mark.asyncio
 async def test_web_socket_send_message(test_client):
     websocket_manager = WebSocketManager()
-    
+
     @app.websocket_route('/websocket_connect')
     async def connect(websocket: WebSocket):
         websocket_manager.connections[uuid.uuid4()] = websocket
         await websocket.accept()
-         
 
     with test_client.websocket_connect('/websocket_connect') as websocket:
-        assert 1 == 1
-        #await websocket_manager.send_message(
-        #    list(websocket_manager.connections)[0], "test_message")
-        #assert websocket.receive_json() == "test_message"
-        
+        await websocket_manager.send_message(
+            list(websocket_manager.connections)[0], "test_message")
+        assert websocket.receive_json() == "test_message"
     await websocket_manager.close_and_remove_all_connections()
-
