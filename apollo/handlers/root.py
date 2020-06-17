68a6046e353ef9647f2bb16ee3c82d621d8415c7
Robin Siep <RobinSiep1@gmail.com>
2020-06-17 15:03:48 +0200

    custom logging config

--- apollo/handlers/root.py
+++ apollo/handlers/root.py
@@ -1,10 +1,14 @@
+import logging
+
 from apollo.lib.router import SecureRouter
 from apollo.lib.schemas.version import VersionSchema
 from apollo.lib.version import version
 
+log = logging.getLogger(__name__)
 router = SecureRouter()
 
 
 @router.get("/", response_model=VersionSchema)
 def get_root():
+    logging.critical('handler')
     return {'version': version}
