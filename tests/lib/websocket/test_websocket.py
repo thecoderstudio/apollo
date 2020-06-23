584c9048adb6958e5e20c22672859d183ec1987a
Robin Siep <RobinSiep1@gmail.com>
2020-06-23 16:37:09 +0200

    get agent tests

--- tests/lib/websocket/test_websocket.py
+++ tests/lib/websocket/test_websocket.py
@@ -13,5 +13,36 @@ async def test_connect_agent(mocker):
 
     await manager.connect_agent(mock_agent_id, websocket_mock)
 
-    assert manager.open_agent_connections[mock_agent_id] == websocket_mock
+    assert manager.open_agent_connections[mock_agent_id] is websocket_mock
     websocket_mock.accept.assert_awaited_once()
+
+
+@pytest.mark.asyncio
+async def test_connect_user(mocker):
+    websocket_mock = mocker.patch('fastapi.WebSocket', autospec=True)
+    manager = WebSocketManager()
+
+    connection_id = await manager.connect_user(websocket_mock)
+
+    assert manager.open_user_connections[connection_id] is websocket_mock
+    websocket_mock.accept.assert_awaited_once()
+
+
+@pytest.mark.asyncio
+async def test_get_agent_connection(mocker):
+    websocket_mock = mocker.patch('fastapi.WebSocket', autospec=True)
+    mock_agent_id = uuid.uuid4()
+    manager = WebSocketManager()
+    await manager.connect_agent(mock_agent_id, websocket_mock)
+
+    connection = manager.get_agent_connection(mock_agent_id)
+
+    assert connection is websocket_mock
+
+
+@pytest.mark.asyncio
+async def test_get_agent_connection_not_found():
+    manager = WebSocketManager()
+
+    with pytest.raises(KeyError):
+        await manager.get_agent_connection(uuid.uuid4())
