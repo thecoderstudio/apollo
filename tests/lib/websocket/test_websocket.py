f14d46e2ea7e045d4c7db7071ffee36c2b5995c4
Robin Siep <RobinSiep1@gmail.com>
2020-06-24 09:55:47 +0200

    complete manager tests

--- tests/lib/websocket/test_websocket.py
+++ tests/lib/websocket/test_websocket.py
@@ -65,3 +65,46 @@ async def test_get_user_connection_not_found():
 
     with pytest.raises(KeyError):
         await manager.get_user_connection(uuid.uuid4())
+
+
+@pytest.mark.asyncio
+async def test_message_agent(mocker):
+    websocket_mock = mocker.patch('fastapi.WebSocket', autospec=True)
+    mock_agent_id = uuid.uuid4()
+    sender_id = uuid.uuid4()
+    manager = WebSocketManager()
+    await manager.connect_agent(mock_agent_id, websocket_mock)
+
+    await manager.message_agent(sender_id, mock_agent_id, 'test')
+
+    websocket_mock.send_json.assert_awaited_once_with({
+        'connection_id': str(sender_id),
+        'message': 'test'
+    })
+
+
+@pytest.mark.asyncio
+async def test_message_agent_not_found():
+    manager = WebSocketManager()
+
+    with pytest.raises(KeyError):
+        await manager.message_agent(uuid.uuid4(), uuid.uuid4(), 'test')
+
+
+@pytest.mark.asyncio
+async def test_message_user(mocker):
+    websocket_mock = mocker.patch('fastapi.WebSocket', autospec=True)
+    manager = WebSocketManager()
+    connection_id = await manager.connect_user(websocket_mock)
+
+    await manager.message_user(connection_id, 'test')
+
+    websocket_mock.send_text.assert_awaited_once_with('test')
+
+
+@pytest.mark.asyncio
+async def test_message_user_not_found():
+    manager = WebSocketManager()
+
+    with pytest.raises(KeyError):
+        await manager.message_user(uuid.uuid4(), 'test')
