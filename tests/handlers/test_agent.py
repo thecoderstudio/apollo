ea4a1cfbc49db93b3a4e396b0a79392b753fccab
Robin Siep <RobinSiep1@gmail.com>
2020-06-24 16:55:35 +0200

    shell success test

--- tests/handlers/test_agent.py
+++ tests/handlers/test_agent.py
@@ -1,8 +1,11 @@
 import uuid
+from unittest.mock import patch
 
 import pytest
+from fastapi import WebSocket
 
 from apollo.lib.exceptions import HTTPException
+from apollo.lib.websocket import WebSocketManager
 
 
 def test_post_agent_success(test_client, session_cookie):
@@ -39,8 +42,25 @@ def test_post_agent_unauthenticated(test_client, db_session):
     assert response.json()['detail'] == "Permission denied."
 
 
-def test_shell(test_client, session_cookie):
-    pass
+@pytest.mark.asyncio
+async def test_shell(mocker, test_client, session_cookie):
+    connection_id = uuid.uuid4()
+    mock_agent_id = uuid.uuid4()
+    agent_websocket_mock = mocker.create_autospec(WebSocket)
+    await WebSocketManager().connect_agent(mock_agent_id, agent_websocket_mock)
+
+    with patch('uuid.uuid4', return_value=connection_id):
+        with test_client.websocket_connect(
+            f"/agent/{mock_agent_id}/shell",
+            cookies=session_cookie
+        ) as websocket:
+            websocket.send_text("test command")
+            websocket.close(code=1000)
+
+    agent_websocket_mock.send_json.assert_awaited_once_with({
+        'connection_id': str(connection_id),
+        'message': "test command"
+    })
 
 
 def test_shell_agent_not_found(test_client, session_cookie):
