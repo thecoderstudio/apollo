154f9f920626d59c2172417a62f98ef17d4c6954
Robin Siep <RobinSiep1@gmail.com>
2020-06-24 12:11:42 +0200

    user connection manager tests

--- tests/lib/websocket/test_user.py
+++ tests/lib/websocket/test_user.py
@@ -1,5 +1,5 @@
 import uuid
-from unittest.mock import AsyncMock, patch
+from unittest.mock import patch
 
 import pytest
 from fastapi import WebSocket
@@ -12,11 +12,11 @@ from apollo.lib.websocket.user import UserConnectionManager
 async def test_user_connection_manager_connect(mocker):
     manager = UserConnectionManager()
     mock_agent_id = uuid.uuid4()
-    agent_websocket_mock = AsyncMock(spec=WebSocket)
+    agent_websocket_mock = mocker.create_autospec(WebSocket)
     await manager.websocket_manager.connect_agent(mock_agent_id,
                                                   agent_websocket_mock)
 
-    user_websocket_mock = AsyncMock(spec=WebSocket)
+    user_websocket_mock = mocker.create_autospec(WebSocket)
     user_websocket_mock.receive_text.side_effect = ["test",
                                                     WebSocketDisconnect]
     with patch(
@@ -26,3 +26,20 @@ async def test_user_connection_manager_connect(mocker):
             user_websocket_mock, mock_agent_id)
         message_agent_mock.assert_awaited_once_with(
             connection_id, mock_agent_id, 'test')
+
+
+@pytest.mark.asyncio
+async def test_user_connection_manager_get_connection(mocker):
+    manager = UserConnectionManager()
+    websocket_mock = mocker.create_autospec(WebSocket)
+    connection_id = await manager.websocket_manager.connect_user(
+        websocket_mock)
+
+    assert manager.get_connection(connection_id) is websocket_mock
+
+
+def test_user_connection_manager_get_connection_not_found():
+    manager = UserConnectionManager()
+
+    with pytest.raises(KeyError):
+        manager.get_connection(uuid.uuid4())
