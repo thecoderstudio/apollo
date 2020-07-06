d79e7f0220f40ea71ce475134373d57da0f0fba5
rikvanderwerf <rikvanderwerf@invyte.app>
2020-07-06 16:56:07 +0200

    updated test

--- tests/lib/test_decorators.py
+++ tests/lib/test_decorators.py
@@ -6,13 +6,12 @@ from apollo.lib.websocket.interest_type import WebSocketObserverInterestType
 
 @pytest.mark.asyncio
 async def test_notify_websockets(mocker):
-
-    mocked = mocker.patch('apollo.lib.websocket.app.AppConnectionManager',
-                          autospec=True)
+    mocked = mocker.patch('apollo.lib.websocket.app.AppConnectionManager.' +
+                          'send_message_to_connections')
 
     @notify_websockets(WebSocketObserverInterestType.AGENT_LISTING)
     def test():
         pass
 
     await test()
-    mocked.assert_called()
+    mocked.assert_called_with(WebSocketObserverInterestType.AGENT_LISTING)
