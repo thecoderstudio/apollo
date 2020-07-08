e90a42b154c9655b1e8b0c9df7017546a90f7cf3
Robin Siep <RobinSiep1@gmail.com>
2020-07-08 11:26:31 +0200

    connection recovery

--- apollo/lib/websocket/user.py
+++ apollo/lib/websocket/user.py
@@ -49,22 +49,60 @@ class UserConnectionManager(ConnectionManager):
         try:
             while True:
                 stdin = await connection.receive_text()
-                await self._message_agent(
-                    target_agent_id,
-                    ShellIOSchema(
-                        connection_id=connection_id,
-                        message=stdin
+                try:
+                    await self._message_agent(
+                        target_agent_id,
+                        ShellIOSchema(
+                            connection_id=connection_id,
+                            message=stdin
+                        )
                     )
-                )
+                except KeyError:
+                    await self._recover_connection(connection_id,
+                                                   target_agent_id)
+
         except WebSocketDisconnect:
             return
 
+    async def _recover_connection(self, connection_id: uuid.UUID,
+                                  target_agent_id: uuid.UUID):
+        time_elapsed = 0
+        while True:
+            if await self._attempt_connection_recovery(connection_id,
+                                                       target_agent_id):
+                return
+
+            await self._send_connection_lost(connection_id, time_elapsed)
+            await asyncio.sleep(1)
+            time_elapsed = time_elapsed + 1
+
+    async def _attempt_connection_recovery(self, connection_id: uuid.UUID,
+                                           target_agent_id: uuid.UUID):
+        try:
+            self.websocket_manager.get_agent_connection(target_agent_id)
+            await self._message_user(
+                connection_id,
+                "\n\r\nConnection recovered\n\r\n"
+            )
+            await self._inform_agent_of_new_connection(
+                connection_id, target_agent_id)
+            return True
+        except KeyError:
+            return False
+
+    async def _send_connection_lost(self, connection_id: uuid.UUID,
+                                    time_elapsed: int):
+        await self._message_user(
+            connection_id,
+            f"\n\r\nConnection to agent lost. Reconnecting.. {time_elapsed}s"
+        )
+
+    async def _message_user(self, connection_id: uuid.UUID, message: str):
+        await self.websocket_manager.message_user(connection_id, message)
+
     async def _message_agent(self, target_agent_id: uuid.UUID,
                              message: BaseMessageSchema):
-        asyncio.create_task(
-            self.websocket_manager.message_agent(
-                target_agent_id, message)
-        )
+        await self.websocket_manager.message_agent(target_agent_id, message)
 
     async def close_connection(self, connection_id: uuid.UUID):
         await self.websocket_manager.close_user_connection(connection_id)
