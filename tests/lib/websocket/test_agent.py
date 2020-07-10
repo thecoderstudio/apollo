16de6bb7d873c08d5384183fee370535f1241a62
Robin Siep <RobinSiep1@gmail.com>
2020-07-10 13:44:19 +0200

    listen and forward test

--- tests/lib/websocket/test_agent.py
+++ tests/lib/websocket/test_agent.py
@@ -113,8 +113,35 @@ async def test_agent_connection_message(websocket_mock):
 
 
 @pytest.mark.asyncio
-async def test_agent_connection_listen_and_forward(websocket_mock):
-    pass
+async def test_agent_connection_listen_and_forward(
+    user_connection_manager,
+    websocket_mock
+):
+    user_connection = UserConnection(websocket_mock)
+    await user_connection_manager.accept_connection(user_connection)
+    agent_connection = AgentConnection(websocket_mock, uuid.uuid4())
+
+    with patch(
+        'apollo.lib.websocket.agent.AgentConnection.receive_json',
+        side_effect=[
+            ShellIOSchema(
+                connection_id=user_connection.id_,
+                message='a'
+            ).dict(),
+            ShellIOSchema(
+                connection_id=user_connection.id_,
+                message='b'
+            ).dict(),
+            WebSocketDisconnect
+        ]
+    ):
+        with patch(
+            'apollo.lib.websocket.user.UserConnection.send_text',
+            wraps=user_connection.send_text
+        ) as send_text:
+            await agent_connection.listen_and_forward()
+
+            send_text.assert_has_awaits([call('a'), call('b')])
 
 
 @pytest.mark.asyncio
