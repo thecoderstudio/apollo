830176f22af1dc1f8529605fc07d1cb8927d9a99
Robin Siep <RobinSiep1@gmail.com>
2020-10-14 16:08:50 +0200

    configurable filename

--- apollo/handlers/agent/action/linpeas.py
+++ apollo/handlers/agent/action/linpeas.py
@@ -1,13 +1,15 @@
-import logging
 import uuid
 
-from fastapi import WebSocket
+from fastapi import Depends, WebSocket
 from fastapi.responses import PlainTextResponse
+from sqlalchemy.orm import Session
 
 from apollo.lib.router import SecureRouter
 from apollo.lib.schemas.message import Command
 from apollo.lib.security import Allow, Authenticated
 from apollo.lib.websocket.action.linpeas import LinPEASManager
+from apollo.models import get_session
+from apollo.models.agent import get_agent_by_id
 
 router = SecureRouter([
     (Allow, Authenticated, 'agent.action')
@@ -24,11 +26,20 @@ async def linpeas(agent_id: uuid.UUID, websocket: WebSocket):
 
 @router.get("/agent/{agent_id}/action/linpeas/export",
             permission='agent.action')
-def export_linpeas(agent_id: uuid.UUID, ansi: bool = False):
+def export_linpeas(
+    agent_id: uuid.UUID,
+    ansi: bool = False,
+    filename: str = None,
+    session: Session = Depends(get_session)
+):
     report = LinPEASManager.get_report(agent_id, ansi)
+    if not filename:
+        agent = get_agent_by_id(session, agent_id)
+        filename = f"LinPEAS-{agent.name}.txt"
+
     return PlainTextResponse(
         headers={
-            'Content-Disposition': "attachment; filename=report.txt"
+            'Content-Disposition': f"attachment; filename={filename}"
         },
         status_code=200,
         content=report
